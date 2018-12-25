c4f57f88c9ff90f362e8bff438780936865d4cf0
oyoui <877252373@qq.com>
2018-12-25 09:36:19 +0800

    add log

--- hello.py
+++ hello.py
@@ -13,4 +13,7 @@ def root():
 def env():
     html = "System Time: \n\n"
     html += datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    import logging  # 引入logging模块
+    logging.basicConfig(level=logging.NOTSET)  # 设置日志级别
+    logging.info(u"%s-----%s"%(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),receive data))
     return Response(html, mimetype='text/plain')
